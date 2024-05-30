a19aebe5fedc283a4c487a58c2164bbf495ed93a
Muhammed Aslam <muhammedaslam204@gmail.com>
2024-05-30 12:37:30 +0530

    ternary operators

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
 const isAuthenticated = false;
 
 const App = () => {
-  if (isAuthenticated) {
-    return <div>Hello World!</div>;
-  }
-  return <div>You are not logged in</div>;
+  return isAuthenticated ? (
+    <div>Hello World</div>
+  ) : (
+    <div>You are not logged in</div>
+  );
 };
-
 export default App;
