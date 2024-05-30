c127413348c6de5dc90700777403542c12843495
Muhammed Aslam <muhammedaslam204@gmail.com>
2024-05-30 12:33:48 +0530

    removed else as it is not needed

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,10 @@
-const isAuthenticated = true;
+const isAuthenticated = false;
 
 const App = () => {
   if (isAuthenticated) {
     return <div>Hello World!</div>;
-  } else {
-    return <div>You are not logged in</div>;
   }
+  return <div>You are not logged in</div>;
 };
 
 export default App;
