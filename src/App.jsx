74b78cf5718eb0be714c966cf60316cbcd9bcd9b
Muhammed Aslam <muhammedaslam204@gmail.com>
2024-05-30 15:57:08 +0530

    event listner and formdata

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,9 @@
 import { Form } from "./Form";
 
 const App = () => {
+  window.addEventListener("resize", () => {
+    console.log(window.innerHeight, window.innerWidth);
+  });
   return <Form />;
 };
 export default App;
--- src/Form.jsx
+++ src/Form.jsx
@@ -1,12 +1,29 @@
 export function Form() {
   return (
     <div>
-      <form>
+      <form
+        onSubmit={(event) => {
+          event.preventDefault();
+
+          const formData = new FormData(event.target);
+
+          const username = formData.get("username");
+          const password = formData.get("password");
+
+          fetch("http://localhost:8000/api/login", {
+            body: {
+              username: { username },
+              password: { password },
+            },
+            method: "POST",
+          });
+        }}>
         <label htmlFor="username">Username</label>
         <br />
         <input
           type="text"
           id="username"
+          name="username"
           onChange={(event) => {
             console.log(`Username: ${event.target.value}`);
           }}
@@ -17,6 +34,7 @@ export function Form() {
         <input
           type="password"
           id="password"
+          name="password"
           onChange={(event) => {
             console.log(`Password: ${event.target.value}`);
           }}
