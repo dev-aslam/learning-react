a3b3aac95ac639bc03d03f4b5b35b15cbf22a0e2
Muhammed Aslam <muhammedaslam204@gmail.com>
2024-05-30 12:45:44 +0530

    switch cases

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,26 @@
-const isAuthenticated = false;
-
 const App = () => {
-  return isAuthenticated ? (
-    <div>Hello World</div>
-  ) : (
-    <div>You are not logged in</div>
-  );
+  const account_Status = "verified";
+  // verified, not-verified, disabled
+
+  switch (account_Status) {
+    case "verified":
+      return <div>Hello</div>;
+    case "not-verified":
+      return <div>Not verified</div>;
+    case "disabled":
+      return <div>disabled</div>;
+    default:
+      return <div>status not set</div>;
+  }
+
+  // if (account_Status == "Verified") {
+  //   return <div>Hello</div>;
+  // } else if (account_Status == "not-verified") {
+  //   return <div>Not verified</div>;
+  // } else if (account_Status == "disabled") {
+  //   return <div>disabled</div>;
+  // } else {
+  //   return <div>status not set</div>;
+  // }
 };
 export default App;
